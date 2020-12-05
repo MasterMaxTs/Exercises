23fd6976c0614bc381977c714e2cb634ea45d220
MasterMaxTs <max86ts@gmail.com>
2020-12-05 12:21:48 +0300

    2.1. Рефакторинг - Расстояние между точками. [#407447]

---
+++ src/main/java/condition/Point.java
@@ -0,0 +1,24 @@
+package condition;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return (Math.sqrt(Math.pow(this.x - that.x, 2)
+                + Math.pow(this.y - that.y, 2)));
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0 , 0);
+        Point b = new Point(0, 2);
+
+        System.out.println("Distance between point A & point B is: "
+                + a.distance(b));
+    }
+}
