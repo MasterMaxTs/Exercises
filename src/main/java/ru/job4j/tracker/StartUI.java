b3714261c1ac8544c708e39e457db0c1c15935c7
MasterMaxTs <max86ts@gmail.com>
2020-12-12 19:02:27 +0300

    9. toString [#407479]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
+        System.out.println(item.toString());
         Tracker tracker = new Tracker();
         tracker.add(item);
         System.out.println(tracker.findById(1).getId() + ", "
