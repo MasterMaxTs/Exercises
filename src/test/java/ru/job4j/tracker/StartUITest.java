19d2908ec4df198081dac56b64e46e796f158886
MasterMaxTs <max86ts@gmail.com>
2020-12-17 19:01:08 +0300

    5.1. Тестирование. Подготовка данных. [#407409]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.*;
 
 public class StartUITest {
@@ -16,4 +17,26 @@ public class StartUITest {
         Item expect = new Item("MyItemName");
         assertThat(created.getName(), is(expect.getName()));
     }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Replace Item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "Replace Item"};
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("Replace Item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Delete Item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "Delete Item"};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        assertThat(tracker.findById(item.getId()), is(nullValue()));
+    }
+
 }
