01606842b1de44c08165173328a0f2458f74a221
MasterMaxTs <max86ts@gmail.com>
2021-01-13 13:42:05 +0300

    6. Изменить программу Tracker из 2-го модуля [#407384]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -32,6 +32,7 @@ public class Tracker {
         for (int index = 0; index < items.size(); index++) {
             if (items.get(index).getId() == id) {
                 rsl = index;
+                break;
             }
         }
         return rsl;
