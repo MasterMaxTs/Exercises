cf3fd49b48f1abbad3f9a822315e9dc480524dde
Maxim Tsurkanov <max86ts@gmail.com>
2021-01-23 22:51:45 +0300

    2. Travis CI [#407364]

--- src/main/java/inheritance/ReportUsage.java
+++
@@ -1,9 +0,0 @@
-package inheritance;
-
-public class ReportUsage {
-    public static void main(String[] args) {
-        JSONReport jsreport = new JSONReport();
-        String text = jsreport.generate("name", "body");
-        System.out.println(text);
-    }
-}
