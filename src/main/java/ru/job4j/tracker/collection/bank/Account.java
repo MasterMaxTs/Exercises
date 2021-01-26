fc322976819b47e92f96d5ff79f57237d69f11ee
Maxim Tsurkanov <max86ts@gmail.com>
2021-01-26 13:00:51 +0300

    3. Что такое JavaDoc [#407366]

--- src/main/java/ru/job4j/tracker/collection/bank/Account.java
+++ src/main/java/ru/job4j/tracker/collection/bank/Account.java
@@ -2,31 +2,69 @@ package ru.job4j.tracker.collection.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель банковского счета пользователя в банке
+ * @author MAXIM TSURKANOV
+ * @version 1.0
+ */
 public class Account {
+    /**
+     * Поле, описывающие свойство реквизита счёта
+     */
     private String requisite;
+    /**
+     * Поле, описывающие свойство денежного баланса на счёте
+     */
     private double balance;
 
+    /**
+     * Инициализация полей модели банковского счета с помощью конструктора
+     * @param requisite поле реквизита счета
+     * @param balance поле денежного баланса на счёте
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * Геттер
+     * @return возвращает значение поля реквизита счёта
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Сеттер
+     * @param requisite передает и инициализирует новым значением поле реквизит счета
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Геттр
+     * @return возвращает значение поля денежный баланс на счете
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Сеттер
+     * @param balance передает и инициализирует новым значением поле денежный баланс на счете
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Метод определяет уникальность клиента по полю реквизит счета
+     * @param o ссылка на входящий объект
+     * @return возвращает true, если банковский счет с переданными рекзизитами существует
+     * и false в обратном случае
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +77,10 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Метод возвращает уникальное значение хэш-кода банковского счета
+     * @return возвращает значение хэш-кода клиентского банковского счета
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
--- src/main/java/ru/job4j/tracker/collection/bank/BankService.java
+++ src/main/java/ru/job4j/tracker/collection/bank/BankService.java
@@ -4,17 +4,45 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ *Класс описывает модель банковской системы.
+ * В системе можно производить следующие действия:
+ * 1. Регистрировать пользователя.
+ * 2. Удалять пользователя из системы.
+ * 3. Добавлять пользователю банковский счет. У пользователя системы могут быть несколько счетов.
+ * 4. Переводить деньги с одного банковского счета на другой счет.
+ * @author  MAXIM TSURKANOV
+ * @version 1.0
+ */
 public class BankService {
+    /**
+     * Хранение данных осуществляется в коллекции типа HashMap
+     */
     private HashMap<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод позволяет добавить нового пользователя в систему.
+     * Если пользователь уже есть в системе, то добавление не производится.
+     * @param user пользователь, которого будет добавляться в систему
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Метод позволяет выполнить удаление пользователя из системы
+     * @param user пользователь, который будет удален
+     */
     public void removeUser(User user) {
         users.remove(user);
     }
 
+    /**
+     * Метод позволяет найти пользователя в системе по номеру паспорта
+     * @param passport номер паспорта, по которому производится поиск пользователя
+     * @return возвращает пользователя, у которого номер паспорта совпадает с запрошенным.
+     * Если такого пользователя не нашлось, возвращается null.
+     */
     public User findByPassport(String passport) {
         User rsl = null;
         for (User user
@@ -26,6 +54,12 @@ public class BankService {
         return rsl;
     }
 
+    /**
+     * Метод позволяет добавить новый счет пользователю.
+     * Если добавляемый счет уже есть в списке счетов пользователя, то добавление не производится.
+     * @param passport номер паспорта, по которому производится поиск пользователя в системе
+     * @param account номер счета, который будет добавлен к списку счетов пользователя.
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null && !users.get(user).contains(account)) {
@@ -33,6 +67,13 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод позволяет выполнить поиск счета пользователя по запращиваемым реквизитам
+     * @param passport номер паспорта, по которому производится поиск пользователя в системе
+     * @param requisite запращиваемые реквизиты счета пользователя
+     * @return возвращает счет пользователя, реквизиты которого совпадат с запращиваемыми и null
+     * в противоположном случае
+     */
     public Account findByRequisite(String passport, String requisite) {
         Account rsl = null;
         User user = findByPassport(passport);
@@ -48,6 +89,17 @@ public class BankService {
         return rsl;
     }
 
+    /**
+     * Метод позволяет выполнить перечисление средств с одного счета на другой.
+     * Транзакция будет выполнена, если пользователи с такими счетами найдены и на счету,
+     * с которого оуществляется перечисление, достаточно денежных средств
+     * @param srcPassport номер паспорта пользователя, который выполняет перевод средств
+     * @param srcRequisite реквизиты счета, с которого быдет выполняться перечисление средств
+     * @param destPassport номер паспорта пользователя, который будет получать переводимые средства
+     * @param destRequisite реквизиты счета, на который будут зачисляться средства
+     * @param amount количество переводимых средств в денежном эквиваленте
+     * @return возвращае true, если транзакция прошла успено, и false в противоположном случае
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
