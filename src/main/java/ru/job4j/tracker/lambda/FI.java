06a7477ff74c567e5c019d0495af6c8c00454462
Maxim Tsurkanov <max86ts@gmail.com>
2021-02-04 20:55:00 +0300

    Сортировка массива с помощью компоратора

---
+++ src/main/java/ru/job4j/tracker/lambda/FI.java
@@ -0,0 +1,34 @@
+package ru.job4j.tracker.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FI {
+    public void showInfo(Attachment[] att) {
+        for (Attachment val :
+                att) {
+            System.out.println(val);
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Attachment[] attachments = {
+                new Attachment("imageOne", 200),
+                new Attachment("imageThree", 50),
+                new Attachment("imageTwo", 100),
+        };
+       new FI().showInfo(attachments);
+        /**
+         * Сортировка массива по убыванию по полю size.
+         * Компортатор определяется через анонимный класс
+         */
+        Arrays.sort(attachments, new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment o1, Attachment o2) {
+                return o2.getSize() - o1.getSize();
+            }
+        });
+        new FI().showInfo(attachments);
+    }
+
+}
