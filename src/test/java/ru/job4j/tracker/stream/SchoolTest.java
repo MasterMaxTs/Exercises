3026b0b3c45090806eebb05407a8302fc52968d4
Maxim Tsurkanov <max86ts@gmail.com>
2021-02-08 23:45:03 +0300

    1. Фильтрация учеников [#407212]

--- src/test/java/ru/job4j/tracker/stream/SchoolTest.java
+++ src/test/java/ru/job4j/tracker/stream/SchoolTest.java
@@ -1,4 +1,64 @@
+package ru.job4j.tracker.stream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
 public class SchoolTest {
-  
+    private List<Student> students = new ArrayList<>();
+
+    @Before
+    public void whenSetUp() {
+        students.add(new Student(10, "Surname1"));
+        students.add(new Student(20, "Surname2"));
+        students.add(new Student(30, "Surname3"));
+        students.add(new Student(40, "Surname4"));
+        students.add(new Student(50, "Surname5"));
+        students.add(new Student(60, "Surname6"));
+        students.add(new Student(70, "Surname7"));
+        students.add(new Student(80, "Surname8"));
+        students.add(new Student(90, "Surname9"));
+    }
+
+    @Test
+    public void whenCollectClassA() {
+        School sc = new School();
+        List<Student> rsl = sc.collect(students, s -> s.getScore() >= 70);
+        List<Student> expected = new ArrayList<>();
+        expected.add(new Student(70, "Surname7"));
+        expected.add(new Student(80, "Surname8"));
+        expected.add(new Student(90, "Surname9"));
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenCollectClassB() {
+        School sc = new School();
+        Predicate<Student> first = s -> s.getScore() >= 50;
+        Predicate<Student> second = s -> s.getScore() < 70;
+        Predicate<Student> combine = first.and(second);
+        List<Student> rsl = sc.collect(students, combine);
+        List<Student> expected = new ArrayList<>();
+        expected.add(new Student(50, "Surname5"));
+        expected.add(new Student(60, "Surname6"));
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenCollectClassC() {
+        School sc = new School();
+        List<Student> rsl = sc.collect(students, s -> s.getScore() < 50);
+        List<Student> expected = new ArrayList<>();
+        expected.add(new Student(10, "Surname1"));
+        expected.add(new Student(20, "Surname2"));
+        expected.add(new Student(30, "Surname3"));
+        expected.add(new Student(40, "Surname4"));
+        assertThat(rsl, is(expected));
+    }
 }
