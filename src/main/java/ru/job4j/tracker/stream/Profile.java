43a99e34ebd238c6c7f9fcc38b41c041374fdb40
Maxim Tsurkanov <max86ts@gmail.com>
2021-02-10 16:13:12 +0300

    2. Список адресов [#407213]

--- src/main/java/ru/job4j/tracker/stream/Profile.java
+++ src/main/java/ru/job4j/tracker/stream/Profile.java
@@ -1,9 +1,5 @@
 package ru.job4j.tracker.stream;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Profile {
     private Address address;
 
@@ -11,16 +7,7 @@ public class Profile {
         this.address = address;
     }
 
-    public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream()
-                .map(p -> p.address).collect(Collectors.toList());
-    }
-
-    public List<Address> newCollect(List<Profile> profiles) {
-        return profiles.stream()
-                .map(p -> p.address)
-                .sorted(Comparator.comparing(Address::getCity))
-                .distinct()
-                .collect(Collectors.toList());
+    public Address getAddress() {
+        return address;
     }
 }
--- src/test/java/ru/job4j/tracker/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/tracker/stream/ProfilesTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
 
-public class ProfileTest {
+public class ProfilesTest {
     private List<Profile> profiles = new ArrayList<>();
 
     @Before
@@ -20,31 +20,15 @@ public class ProfileTest {
         Profile profile2 = new Profile();
         Address address2 = new Address("Ukraine", "Komsomolskaya", 34, 5);
         profile2.setAddress(address2);
-        Profile profile3 = new Profile();
-        Address address3 = new Address("Russia", "Krasnaya", 154, 65);
-        profile3.setAddress(address3);
-        Profile profile4 = new Profile();
-        Address address4 = new Address("Kazakhstan", "Zhloby", 4, 90);
-        profile4.setAddress(address4);
         profiles.add(profile1);
         profiles.add(profile2);
-        profiles.add(profile3);
-        profiles.add(profile4);
     }
 
     @Test
     public void whenGetAddressFromProfile() {
-        Profile pr = new Profile();
+        Profiles pr = new Profiles();
         List<Address> rsl = pr.collect(profiles);
         assertThat(rsl.get(0).getCity(), is("Russia"));
         assertThat(rsl.get(1).getHome(), is(34));
-    }
-
-    @Test
-    public void whenGetAddressWithoutDuplicateAndSorted() {
-        Profile pr = new Profile();
-        List<Address> rsl = pr.newCollect(profiles);
-        assertThat(rsl.size(), is(3));
-        assertThat(rsl.get(0).getCity(), is("Kazakhstan"));
     }
 }
