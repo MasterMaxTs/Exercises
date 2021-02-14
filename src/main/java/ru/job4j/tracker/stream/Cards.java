40ebd148a7455c4354a34b904e32513fe7495b8a
Maxim Tsurkanov <max86ts@gmail.com>
2021-02-14 19:49:25 +0300

    8. Перенос методов в Stream API [#407219]

---
+++ src/main/java/ru/job4j/tracker/stream/Cards.java
@@ -0,0 +1,35 @@
+package ru.job4j.tracker.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Cards {
+    private Suit suit;
+    private Value value;
+
+    public Cards(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public List<Cards> generateCards() {
+         return Stream.of(Suit.values())
+                      .flatMap(suit -> Stream.of(Value.values())
+                      .map(value -> new Cards(suit, value)))
+                      .collect(Collectors.toList());
+    }
+
+    @Override
+    public String toString() {
+        return "Cards{" +
+                "suit=" + suit +
+                ", value=" + value +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Cards cards = new Cards(Suit.CLUBS, Value.V_8);
+        cards.generateCards().forEach(System.out::println);
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/stream/Suit.java
@@ -0,0 +1,6 @@
+package ru.job4j.tracker.stream;
+
+public enum Suit {
+    DIAMONDS, HEARTS, SPADES, CLUBS
+}
+
