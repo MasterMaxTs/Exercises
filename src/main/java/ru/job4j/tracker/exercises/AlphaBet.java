973dd5320d79d0d563f15516aeeb974ffae8b798
Maxim Tsurkanov <max86ts@gmail.com>
2021-02-25 00:59:44 +0300

    Collections exercise

---
+++ src/main/java/ru/job4j/tracker/exercises/AlphaBet.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker.exercises;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class AlphaBet {
+    public static String reformat(String s) {
+        String[] subStr = s.split("");
+        List<String> list = Arrays.asList(subStr);
+        list.sort(Comparator.naturalOrder());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            stringBuilder.insert(i, list.get(i));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "websecurityconfigureradapter";
+        System.out.println("Начальная строка:" + str);
+        System.out.println("Строка по возрастанию букв: " + reformat(str));
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/exercises/GeomProgress.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker.exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeomProgress {
+    public static int generateAndSum(int first, int denominator, int count) {
+        List<Integer> list = new ArrayList<>(count);
+        list.add(first);
+        int sum = list.get(0);
+        for (int i = 2; i <= count; i++) {
+            list.add(first * (int) Math.pow(denominator, i - 1));
+            sum += list.get(i - 1);
+        }
+        for (int value :
+                list) {
+            System.out.print(value + " ");
+        }
+
+        System.out.println("Сумма членов: " + sum);
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        generateAndSum(1, 3, 10);
+        generateAndSum(1, -1, 10);
+    }
+}
