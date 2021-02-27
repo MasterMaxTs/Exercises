70956ccb7be65ce0ffda1536d9877f56024045d4
Maxim Tsurkanov <max86ts@gmail.com>
2021-02-27 13:01:31 +0300

    7. Найти символ который используется в строке наибольшее количество раз

---
+++ src/main/java/ru/job4j/tracker/exercises/MostUsedCharacter.java
@@ -0,0 +1,32 @@
+package ru.job4j.tracker.exercises;
+
+import java.util.*;
+
+public class MostUsedCharacter {
+    public static char getMaxCount(String str) {
+        char rsl = ' ';
+        String strLowerCase = str.toLowerCase();
+        String[] strArr = strLowerCase.split(" ");
+        String resultStr = "";
+        for (String value :
+                strArr) {
+            resultStr += value;
+        }
+        char[] charArr = resultStr.toCharArray();
+        Map<Character, Integer> map = new TreeMap<>();
+        for (Character ch :
+                charArr) {
+            map.computeIfAbsent(ch, value -> 1);
+            map.computeIfPresent(ch, (k, v) -> v + 1);
+        }
+        Collection<Integer> col = map.values();
+        int max = Collections.max(col);
+        for (Character key :
+                map.keySet()) {
+            if (map.get(key) == max) {
+                return key;
+            }
+        }
+        return rsl;
+    }
+}
