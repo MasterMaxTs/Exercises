197dddc8e7ac101d7c9a494d750f1aa4eee741dc
Maxim Tsurkanov <max86ts@gmail.com>
2021-03-04 12:24:13 +0300

    2.7. Способы сортировки. Сортировка Map.

---
+++ src/main/java/ru/job4j/tracker/exercises/SortMap.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker.exercises;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class SortMap {
+    public static Map<Integer, Integer> sort (Map<Integer, Integer> map) {
+        Map<Integer, Integer> rsl = new TreeMap<>(Comparator.reverseOrder());
+        rsl.putAll(map);
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/exercises/SortMapTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.tracker.exercises;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class SortMapTest {
+
+    @Test
+    public void sort() {
+        Map<Integer, Integer> input = Map.of(
+                1, 2,
+                7, 4,
+                5, 5
+        );
+        Map<Integer, Integer> rsl = SortMap.sort(input);
+        Map<Integer, Integer> expected = Map.of(
+                7, 4,
+                5, 5,
+                1, 2);
+        assertEquals(rsl, expected);
+    }
+}
