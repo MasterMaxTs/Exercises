f056167566f80de594758b1a4cb41b04e295417f
Maxim Tsurkanov <max86ts@gmail.com>
2021-03-06 19:40:33 +0300

    5.6. Optional. Методы orElseThrow()

---
+++ src/test/java/ru/job4j/tracker/exercises/OptionalOrElseThrowTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker.exercises;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import ru.job4j.tracker.exercises.OptionalOrElseThrow.User;
+import ru.job4j.tracker.exercises.OptionalOrElseThrow.UserNotFoundException;
+
+import java.util.List;
+
+public class OptionalOrElseThrowTest {
+
+    @Test
+    public void whenLoginExist() {
+        List<User> users = List.of(
+                new User("loginOne"),
+                new User("loginTwo")
+        );
+        String rsl = UserNotFoundException.orElseThrow(users, "loginTwo").getLogin();
+        String expected = "loginTwo";
+        assertEquals(rsl, expected);
+    }
+
+    @Test (expected = UserNotFoundException.class)
+    public void whenLoginNotExist() {
+        List<User> users = List.of(
+                new User("loginOne"),
+                new User("loginTwo")
+        );
+        User rsl = UserNotFoundException.orElseThrow(users, "loginThree");
+    }
+
+}
