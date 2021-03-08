064b8e89e3f8d81986170b94194bd9874f9e2822
Maxim Tsurkanov <max86ts@gmail.com>
2021-03-09 00:23:55 +0300

    5.8. Промежуточные операции. Метод flatMap()

---
+++ src/main/java/ru/job4j/tracker/exercises/stream/FlatMapForObject.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker.exercises.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FlatMapForObject {
+    /**
+     * Метод собирает данные из вложенных списков и складывает в один список
+     * @param lists вложенный список
+     * @return возвращает список всех элементов
+     */
+    public static List<Integer> flatMap (List<List<Integer>> lists) {
+        return lists.stream()
+                .flatMap(sl -> sl.stream())
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/exercises/stream/FlatMapForObjectTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.exercises.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlatMapForObjectTest {
+
+    @Test
+    public void flatMap() {
+        List<List<Integer>> lists = List.of(List.of(1, 2) , List.of(3, 4, 5));
+        List<Integer> expect = List.of(1, 2, 3, 4, 5);
+        List<Integer> rsl = FlatMapForObject.flatMap(lists);
+        assertEquals(expect, rsl);
+    }
+}
