f21fcd69db97765692abe3a8e77d3eff97a35681
Maxim Tsurkanov <max86ts@gmail.com>
2021-03-08 21:07:21 +0300

    0.4. Понятие редукции. Метод reduce(). Редукция с начальным значением

---
+++ src/test/java/ru/job4j/tracker/exercises/stream/ReductionWithTwoArgumentsTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.tracker.exercises.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReductionWithTwoArgumentsTest {
+
+    @Test
+    public void collect() {
+        assertEquals(
+                Integer.valueOf(5), ReductionWithTwoArguments.collect(List.of())
+        );
+        assertEquals(
+                Integer.valueOf(5), ReductionWithTwoArguments.collect(List.of(1))
+        );
+        assertEquals(
+                Integer.valueOf(10), ReductionWithTwoArguments.collect(List.of(1, 2))
+        );
+        assertEquals(
+                Integer.valueOf(30), ReductionWithTwoArguments.collect(List.of(1, 2, 3))
+        );
+    }
+}
