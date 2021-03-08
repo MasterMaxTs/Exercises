5e60f7d068dea7921a63f4ef3d79c2003efb0d90
Maxim Tsurkanov <max86ts@gmail.com>
2021-03-08 23:36:19 +0300

    1.5. Создание стрима. Метод Stream.builder()

---
+++ src/test/java/ru/job4j/tracker/exercises/stream/StreamBuilderTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.tracker.exercises.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+
+public class StreamBuilderTest {
+
+    @Test
+    public void createStream() {
+        Integer[] data = {1, 2, 3};
+        List<Integer> expected = List.of(1, 3);
+        Stream<Object> stream = StreamBuilder.createStream(data);
+        List<Integer> rsl = stream
+                                 .map(v -> (Integer) v)
+                                 .collect(Collectors.toList());
+        assertEquals(expected, rsl);
+    }
+}
