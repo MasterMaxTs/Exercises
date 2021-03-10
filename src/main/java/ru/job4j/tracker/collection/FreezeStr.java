04cc0970a410ebf5e522f9627b8338ae1de37662
Maxim Tsurkanov <max86ts@gmail.com>
2021-03-10 20:55:06 +0300

    2. Проверить две строчки на идентичность.

--- src/main/java/ru/job4j/tracker/collection/FreezeStr.java
+++ src/main/java/ru/job4j/tracker/collection/FreezeStr.java
@@ -4,28 +4,25 @@ import java.util.*;
 
 public class FreezeStr {
     public static boolean eq(String left, String right) {
-        Map<Character, Integer> hml = createMap(left);
-        Map<Character, Integer> hmr = createMap(right);
-        if (hml.size() != hmr.size()) {
-            return false;
-        }
-        for (Character key :
-                hml.keySet()) {
-            if (!hml.get(key).equals(hmr.get(key))) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static Map<Character, Integer> createMap(String string) {
-        Map<Character, Integer> hm= new HashMap<>();
-        char[]leftChars = string.replaceAll("\\s", "").toCharArray();
+        Map<Character, Integer> hm = new HashMap<>();
+        char[] leftChars = left.toCharArray();
         for (Character ch :
                 leftChars) {
-            hm.putIfAbsent(ch, 1);
             hm.computeIfPresent(ch, (k, v) -> v + 1);
+            hm.putIfAbsent(ch, 1);
         }
-        return hm;
+        char[] rightChars = right.toCharArray();
+        for (Character ch :
+                rightChars) {
+            if (!hm.containsKey(ch)) {
+                return false;
+            } if (hm.get(ch) == 1) {
+                hm.remove(ch);
+                continue;
+            }
+            hm.replace(ch, hm.get(ch) - 1);
+        }
+        return hm.isEmpty();
     }
 }
+
