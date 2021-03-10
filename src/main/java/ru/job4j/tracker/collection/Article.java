36d275c6d64527b71b4082abe262aafd9df3ba71
Maxim Tsurkanov <max86ts@gmail.com>
2021-03-10 19:50:37 +0300

    1. Генератор текста.

--- src/main/java/ru/job4j/tracker/collection/Article.java
+++ src/main/java/ru/job4j/tracker/collection/Article.java
@@ -2,16 +2,20 @@ package ru.job4j.tracker.collection;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        String OriginFormat = origin.replaceAll("[,.!?;(){}<>]", "");
-        List<String> listOrigin = Arrays.asList(OriginFormat.split(" "));
-        Set<String> hso = new HashSet<>(listOrigin);
-        List<String> listLine = Arrays.asList(line.split(" "));
-        Set<String> hsl = new HashSet<>(listLine);
-        return hso.containsAll(hsl);
+        String originFormat = origin.replaceAll("[,.!?;:(){}<>]", "");
+        String[] originArr = originFormat.split(" ");
+        Set<String> hs = new HashSet<>(Arrays.asList(originArr));
+        String[] lineArr = line.split(" ");
+        for (String str :
+                lineArr) {
+            if (!hs.contains(str)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
