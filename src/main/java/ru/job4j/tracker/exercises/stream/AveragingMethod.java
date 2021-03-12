135f7e37c9f671249ef5cb2b97da9c0ba190c172
Maxim Tsurkanov <max86ts@gmail.com>
2021-03-12 23:30:23 +0300

    2.4. Группировка элементов и среднее значение. Метод averagingDouble(). Добавлен метод для решения задачи 2.2. Группировка элементов и подсчет количества. Метод counting(). Переработаны тесты

--- src/main/java/ru/job4j/tracker/exercises/stream/AveragingMethod.java
+++ src/main/java/ru/job4j/tracker/exercises/stream/AveragingMethod.java
@@ -16,6 +16,13 @@ public class AveragingMethod {
         public String getName() {
             return name;
         }
+
+        @Override
+        public String toString() {
+            return "Company"
+                    + " name='" + name
+                    + '\'';
+        }
     }
 
     public static class Worker {
@@ -33,6 +40,16 @@ public class AveragingMethod {
 
         public int getAge() {
             return age;
+        }
+
+        @Override
+        public String toString() {
+            return "Worker{"
+                    + company
+                    + " - "
+                    + age
+                    + " age"
+                    + '}';
         }
     }
 
@@ -52,19 +69,42 @@ public class AveragingMethod {
                 );
     }
 
+    /**
+     *
+     * @param workers список объектов на входе
+     * @return возращает отсортированную Мар, где ключ - имя компании,
+     * значение - количество сотрудников компании
+     */
+    public static Map<String, Long> groupAndCount(List<Worker> workers) {
+        return workers.stream()
+                .collect(Collectors.groupingBy(
+                        w -> w.getCompany().getName(),
+                        TreeMap::new,
+                        Collectors.counting()
+                        )
+                );
+    }
+
     public static void main(String[] args) {
         Company c1 = new Company("Google");
         Company c2 = new Company("Microsoft");
         Company c3 = new Company("Apple");
         Worker w1 = new Worker(c1, 27);
         Worker w2 = new Worker(c2, 23);
-        Worker w3 = new Worker(c3, 20);
+        Worker w3 = new Worker(c3, 19);
         Worker w4 = new Worker(c1, 23);
         Worker w5 = new Worker(c2, 18);
-        Worker w6 = new Worker(c3, 22);
-        List<Worker> workers = List.of(w1, w2, w3, w4, w5, w6);
+        Worker w6 = new Worker(c3, 21);
+        Worker w7 = new Worker(c2, 28);
+        Worker w8 = new Worker(c3, 20);
+        Worker w9 = new Worker(c3, 22);
+        List<Worker> workers = List.of(w1, w2, w3, w4, w5, w6, w7, w8, w9);
+        workers.forEach(System.out::println);
         averaging(workers).forEach(
                 (k, v) -> System.out.println( k + " : " + v + " average age")
+        );
+        groupAndCount(workers).forEach(
+                (k, v) -> System.out.println( k + " : " + v + " count of workers")
         );
     }
 }
--- src/test/java/ru/job4j/tracker/exercises/stream/AveragingMethodTest.java
+++ src/test/java/ru/job4j/tracker/exercises/stream/AveragingMethodTest.java
@@ -1,34 +1,52 @@
 package ru.job4j.tracker.exercises.stream;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import ru.job4j.tracker.exercises.stream.AveragingMethod.Company;
 import ru.job4j.tracker.exercises.stream.AveragingMethod.Worker;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class AveragingMethodTest {
+    private List<Worker> workers = new ArrayList<>();
 
-    @Test
-    public void averaging() {
+    @Before
+    public void setUp() {
         Company c1 = new Company("Apple");
         Company c2 = new Company("Microsoft");
         Company c3 = new Company("Google");
-        Worker w1 = new Worker(c1, 27);
-        Worker w2 = new Worker(c2, 23);
-        Worker w3 = new Worker(c3, 20);
-        Worker w4 = new Worker(c1, 23);
-        Worker w5 = new Worker(c2, 31);
-        Worker w6 = new Worker(c3, 22);
-        List<Worker> workers = List.of(w1, w2, w3, w4, w5, w6);
+        workers.add(new Worker(c1, 27));
+        workers.add(new Worker(c2, 23));
+        workers.add(new Worker(c3, 20));
+        workers.add(new Worker(c1, 23));
+        workers.add(new Worker(c2, 33));
+        workers.add(new Worker(c3, 22));
+        workers.add(new Worker(c1, 18));
+        workers.add(new Worker(c2, 25));
+        workers.add(new Worker(c1, 27));
+    }
+
+    @Test
+    public void averaging() {
         Map<String, Double> expect = Map.of(
-                "Apple", 25D,
+                "Apple", 23.75D,
                 "Google", 21D,
                 "Microsoft", 27D
         );
         assertEquals(expect, AveragingMethod.averaging(workers));
+    }
 
+    @Test
+    public void counting() {
+        Map<String, Long> expect = Map.of(
+                "Apple", 4L,
+                "Google", 2L,
+                "Microsoft", 3L
+        );
+        assertEquals(expect, AveragingMethod.groupAndCount(workers));
     }
 }
