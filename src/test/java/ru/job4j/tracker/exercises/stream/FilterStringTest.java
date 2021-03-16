98da3617809b8ad56b69aba345a3e221af237e6b
Maxim Tsurkanov <max86ts@gmail.com>
2021-03-16 12:34:34 +0300

    6.2. Промежуточные операции. Метод filter(). Фильтрация строк

---
+++ src/test/java/ru/job4j/tracker/exercises/stream/FilterStringTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker.exercises.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FilterStringTest {
+
+    @Test
+    public void filter() {
+        List<String> input = List.of("Abc.java", "sdf.exe", "abf.java");
+        assertEquals(List.of("Abc.java"), FilterString.filter(input));
+    }
+}
