6999f2e03b5c97231353aa6c22632eeae819d568
Maxim Tsurkanov <max86ts@gmail.com>
2021-03-18 15:35:17 +0300

    EasyStream [#401629]

---
+++ src/main/java/ru/job4j/tracker/exercises/stream/EasyStream.java
@@ -0,0 +1,55 @@
+package ru.job4j.tracker.exercises.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EasyStream {
+    private List<Integer> list;
+    private List<Integer> buffer = new ArrayList<>();
+
+    static class Builder {
+        private List<Integer> list;
+
+        public Builder buildList(List<Integer> list) {
+            this.list = list;
+            return this;
+        }
+
+        EasyStream build() {
+            EasyStream es = new EasyStream();
+            es.list = list;
+            return es;
+        }
+    }
+
+    public static EasyStream of(List<Integer> source) {
+       return new Builder().buildList(source).build();
+    }
+
+    public EasyStream map(Function<Integer, Integer> fun) {
+        buffer.clear();
+        for (Integer i
+                : list) {
+            buffer.add(fun.apply(i));
+        }
+        return new Builder().buildList(buffer).build();
+    }
+
+    public EasyStream filter(Predicate<Integer> fun) {
+        buffer.clear();
+        for (Integer i
+                : list) {
+            if (fun.test(i)) {
+                buffer.add(i);
+            }
+        }
+        return new Builder().buildList(buffer).build();
+    }
+
+    public List<Integer> collect() {
+        return list;
+    }
+
+}
